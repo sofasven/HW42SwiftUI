87f603f5616f24005e361469be327a5daa3c80c6
Sofa <sofasven00@gmail.com>
2023-12-13 13:11:19 +0300

    Project created

---
+++ HW42SwiftUI/HW42SwiftUI/HW42SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW42SwiftUIApp.swift
+//  HW42SwiftUI
+//
+//  Created by Sofa on 13.12.23.
+//
+
+import SwiftUI
+
+@main
+struct HW42SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
